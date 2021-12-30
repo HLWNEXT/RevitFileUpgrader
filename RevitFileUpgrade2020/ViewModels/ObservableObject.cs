6a595c81ee5b4a1be1550f97909e6ee01cd0317d
Chenzhang Wang <cwang@hlw.com>
2021-12-29 22:05:02 -0500

    Add Revit 2020 support.

---
+++ RevitFileUpgrade2020/ViewModels/ObservableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace RevitFileUpgrade.ViewModels
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
